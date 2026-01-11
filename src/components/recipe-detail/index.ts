5b6a5cdcef252e2d3e87d97c8be856bb535545b3
RCYGM <jurgenmuller997@gmail.com>
2026-01-11 12:33:24 +0100

    refactorizacion: id.tsx

---
+++ src/components/recipe-detail/index.ts
@@ -0,0 +1,7 @@
+export { RecipeHeader } from "./RecipeHeader";
+export { RecipeTitleSection } from "./RecipeTitleSection";
+export { RecipeStats } from "./RecipeStats";
+export { RecipeVideoTutorial } from "./RecipeVideoTutorial";
+export { RecipeIngredients } from "./RecipeIngredients";
+export { RecipeInstructions } from "./RecipeInstructions";
+export { RecipeFavoriteButton } from "./RecipeFavoriteButton";
